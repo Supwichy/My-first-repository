f3a15807416244242b757466dab8d27ff588e3e1
Supawit Changsao <supwichy55@gmail.com>
2022-11-09 16:45:16 +0700

    add num1

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int divide = 2;
         int number = 50;
-        int result = 20;
+        int result = 0;
+        int num1;
     }
 }
