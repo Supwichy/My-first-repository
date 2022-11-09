3de8ab9400bb8b2aa935ec0c6cea10e3bb979400
Supawit Changsao <supwichy55@gmail.com>
2022-11-09 16:41:00 +0700

    add int divide = 2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int divide = 2;
         int number = 50;
         int result = 20;
     }
